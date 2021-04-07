5c7dd6c26812ec22fea7a9a64ac3841de0830757
pvn-leo <pavan.leo36dna@gmail.com>
2021-04-07 17:22:12 +0530

    Shows 1 course of a teacher

--- classroom/urls.py
+++ classroom/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 urlpatterns = [
     path('', views.indexView, name='index'),
     path('teacher/<slug:teacher_id>/<int:choice>/Classes/', views.t_clas, name='t_clas'),
-    path('teacher/<int:assign_id>/Extra_class/', views.t_extra_class, name='t_extra_class'),
 ]
 
 # admin.site.site_url= None
